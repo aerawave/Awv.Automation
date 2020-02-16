8ccff2e4b28bde96a6c3e422d0c976a76297be93
Joshua <jaxkinzer@aerawave.com>
2020-02-16 01:59:44 -0500

    Added FilePathGenerator

---
+++ Awv.Automation.Generation/FilePathGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Awv.Automation.Generation
+{
+    public class FilePathGenerator : QueriedGenerator<string, string>
+    {
+        public static readonly Func<string, IEnumerable<string>> GET_FILES = directory => Directory.GetFiles(directory);
+
+        public FilePathGenerator(string directory) : base (directory, GET_FILES)
+        {
+        }
+
+        public FilePathGenerator(IEnumerable<string> files)
+            : base(files)
+        {
+        }
+    }
+}
